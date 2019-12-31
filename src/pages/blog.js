c8313fca691d49f77b2f4050f8b5a8578029b52e
Ryan Pitts <47765262+RyanPPitts@users.noreply.github.com>
2019-12-30 17:49:30 -0800

    Creating layout component
    Creating layout component that combines header and footer components

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import Footer from '../components/footer';
+import Layout from '../components/layout';
 
 
 const BlogPage = () => {
     return (
-        <div> 
+        <Layout>
             <h1>Blog Page</h1>
             <h2>Posts will show up here ....</h2>
-            <Footer/>
-        </div>
+        </Layout>
     )
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import { Link } from 'gatsby';
-import Footer from '../components/footer';
+import Layout from '../components/layout';
 
 
 const IndexPage = () => {
     return(
-        <div>
+        <Layout>
             <h1>Hello.</h1>
             <h2>I'm Ryan, a full stack developer living in Seattle area</h2>
             {/* <p>Need a developer? <a href="/contact">Contact Me</a></p> */}
             <p>Need a developer? <Link to="/contact">Contact Me.</Link></p>
-            <Footer/>
-        </div>
+        </Layout>
     )
 }
 
